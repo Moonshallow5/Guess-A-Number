cc3df247f57e42bc7ccd18a974fa8cf41bba883d
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-06-27 00:40:08 +0800

    ss

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(computerPicks());
 
+    }
 
+    public static int computerPicks(){
+        return (int)(Math.random()*100);
     }
 
 
