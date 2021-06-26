1d63fee3ee32b0504870af11194fb2df02c4ca61
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-06-26 23:45:03 +0800

    new branch lol

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(computerMakesNumber());
+        
+    }
+    public static int computerMakesNumber(){
+        return (int) (Math.random()*100);
     }
 }
