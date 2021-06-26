cc38d0514926250fd7e95d76fc089c0892b3776f
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-06-26 23:49:18 +0800

    as

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(computerMakesNumber());
-        
     }
-    public static int computerMakesNumber(){
-        return (int) (Math.random()*100);
-    }
+
+    
 }
