af54511a3a4e556018ff36cec979e19514392f54
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-06-27 23:53:00 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -8,17 +8,19 @@ public class Main {
     public static void number(){
          computerNumber =(int)(Math.random()*100);
     }
+    static int human=0;
+    static int computer=0;
     public static void write(){
-        System.out.println("------------------------------------------------------");
+        System.out.println("--------------------------------------------------------------------------");
         System.out.println("Do you want to choose how many tries you get? ");
-        System.out.println("Enter Ya to choose and No to guess with infinite tries");
-        System.out.println("-------------------------------------------------------");
+        System.out.println("Enter Y to choose the number of tries and N to guess with infinite tries");
+        System.out.println("Please enter -1 if you want to concede");
+        System.out.println("--------------------------------------------------------------------------");
     }
     public static void start() {
-        Scanner scanner = new Scanner(System.in);
         write();
-        String x = scanner.nextLine();
-        if (x.equals("Ya")) {
+        char x = InputUtil.readCharFromUser();
+        if (x=='Y') {
             pickPlay();
 
         }else {
@@ -37,10 +39,13 @@ public class Main {
         playAgain();
     }
     public static void pickLoop() {
-        Scanner scanner = new Scanner(System.in);
+
         while (true) {
             System.out.println("Enter");
-            int playerInput = scanner.nextInt();
+            int playerInput = InputUtil.readIntFromUser();
+            if(playerInput==-1){
+                break;
+            }
             inputMore(playerInput);
             pickLess(playerInput);
              if(pickEqual(playerInput)){
@@ -50,10 +55,13 @@ public class Main {
              }
         }
     }
+
     public static void inputMore(int playerInput) {
+
         if (playerInput > computerNumber) {
             System.out.println(playerInput + " is too high");
-            tries++;izz++;
+            tries++;
+            izz++;
         }
     }
     public static boolean pickEqual(int playerInput){
@@ -62,9 +70,9 @@ public class Main {
     }
     public static void pickLess(int playerInput){
           if (playerInput < computerNumber) {
-            System.out.println(playerInput + " is too low");
-            tries++;
-            izz++;
+                  System.out.println(playerInput + " is too low");
+                  tries++;
+                  izz++;
         }
     }
     static int izz=0;
@@ -72,28 +80,37 @@ public class Main {
         number();
         System.out.println(computerNumber);
         playAgain=false;
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter how many tries you want: ");
-        int numberOfTries=scanner.nextInt();
+
+        int numberOfTries=InputUtil.readIntFromUser();
         pickPlayLoop(numberOfTries);
         playAgain();
     }
-    static Scanner scanner=new Scanner(System.in);
+
 
     public static void pickPlayLoop(int numberOfTries){
         while (izz<numberOfTries) {
             System.out.println("Enter a number");
-            int input = scanner.nextInt();
+            int input = InputUtil.readIntFromUser();
+            if(input==-1){
+                computer++;
+                //System.out.println("Score is "+human+" for human and "+computer+" for computer");
+                break;
+            }
             inputMore(input);
              pickLess(input);
              if(pickEqual(input)){
                  tries++;
+                 human++;
                  correct(input);
                  break;
              }
         }if(izz>=numberOfTries){
+            computer++;
             cool(numberOfTries);
         }
+        System.out.println("Score is "+human+" for human and "+computer+" for computer");
     }
 
 
@@ -109,15 +126,17 @@ public class Main {
     }
 
 
-    public static String play(){
-        return "Would you want to play again";
+    public static void play(){
+        System.out.println("Do you want to play again");
+        System.out.println("Enter Y to play again and N to stop playing");
     }
     static int count=1;
     public static void playAgain(){
-        Scanner scanner=new Scanner(System.in);
-        System.out.println(play());
-        String in=scanner.nextLine();
-        playAgain= in.equals("Ya");
+        play();
+        char in=InputUtil.readCharFromUser();
+        if(in=='Y'){
+            playAgain=true;
+        }
         if(!playAgain){
             System.out.println("Good game");
             games();
@@ -131,4 +150,8 @@ public class Main {
     public static void games(){
         System.out.println("Number of games played "+ count);
     }
+    public static void score(){
+
+    }
+
 }
